307284d0212f75c57ecdea94ac842da07a223ceb
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-20 16:55:04 +0000

    app file

---
+++ Vedant/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+const App = () => {
+  return (
+    <div className='w-full min-h-screen'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
