fd242e54a4a927fb955e0f25f53326437fea8837
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-21 11:20:08 +0000

    images loc change

--- Vedant/my-app/src/App.jsx
+++ Vedant/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
 import Con from './components/Con'
+import Spec from './components/Spec'
 const App = () => {
   return (
     <div className='w-full min-h-screen'>
       <Header/>
       <Con/>
+      <Spec/>
     </div>
   )
 }
