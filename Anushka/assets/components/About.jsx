562bfe137cb082ba810d8f2d2c1b23f5975752de
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-22 04:24:15 +0000

    file transfer to SUNA

---
+++ Anushka/assets/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full min-h-screen'>
+    <div className="about">
+        <div  className="w-[250px] rotate-10  "><img className='rounded-4xl' src='f.jpg'></img></div>
+        <div className="content grid ml-[120px]">
+            <div className="text-4xl font-medium  ">About the Instructor</div>
+            <div class="w-[500px] mt-3">
+                Vedant Wedekar brings a wealth of experience in web development, having guided countless students to
+                success in their tech careers. With a background in both teaching and industry expertise, Vedant's passion
+                is to make advanced concepts approachable for everyone.
+            </div>
+            <div className="w-[100px] h-[35px] bg-white rounded-4xl border-1 flex justify-center items-center text-[15px] mt-5"> Learn More</div>
+        </div>
+    </div>
+   
+
+
+
+    </div>
+  )
+}
+export default About
